c30328f16d9866923d3df8d6aaabf42a4d5b4ad8
ddvv <529531385@qq.com>
2023-01-17 15:11:54 +0800

    [UPDATE]统一版本号管理

--- setup.py
+++ setup.py
@@ -9,56 +9,38 @@
 import setuptools
 
 
+def read_version():
+    """
+    读取打包的版本信息
+    """
+    with open("./mmpi/version.py", "r", encoding="utf8") as f:
+        for data in f.readlines():
+            if data.startswith("NEXTB_MMPI_VERSION"):
+                data = data.replace(" ", "")
+                version = data.split("=")[-1][1:-1]
+                return version
+    # 默认返回
+    return "0.2.1"
+
+def read_readme():
+    """
+    读取README信息
+    """
+    with open("./README.md", "r", encoding="utf8") as f:
+        return f.read()
+
 def do_setup(**kwargs):
     try:
         setuptools.setup(**kwargs)
     except (SystemExit, Exception) as e:
         exit(1)
 
-
-long_description = '''
-
-An email detected library. It's support to detect the header, body and attachment.
-It's support to parse the attachment type of ole, rtf, zip and so on.
-
-## Insatall
-
-```
-$ pip install mmpi
-```
-
-mark：`windows` install `yara-python`，download from [here](https://github.com/VirusTotal/yara-python/releases)
-
-## Usage
-
-### 1. Command
-
-```
-$ mmpi-run $email_path
-```
-
-### 2. Quick Start
-
-```
-from mmpi import mmpi
-
-
-def main():
-    emp = mmpi()
-    emp.parse('test.eml')
-    report = emp.get_report()
-    print(report)
-
-
-if __name__ == "__main__":
-    main()
-
-```
-'''
+version = read_version()
+long_description = read_readme()
 
 do_setup(
     name="mmpi",
-    version="0.1.3",
+    version=version,
     author="ddvv",
     author_email="dadavivi512@gmail.com",
     description="email detected library",
